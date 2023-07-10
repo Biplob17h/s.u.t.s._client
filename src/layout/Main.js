51b48de687263e46b163de796a03551cbcc17a87
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-07-10 12:15:41 -0600

    first commit

---
+++ src/layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
