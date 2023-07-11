9dda7ac5f8a6ba110840e376873a395fb22cbf43
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-07-10 21:47:20 -0600

    biplob course json done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { routes } from "./routes/routes";
 function App() {
   return (
     <div className="max-w-xs  overflow-hidden mx-auto lg:max-w-7xl">
-      <RouterProvider router={routes}></RouterProvider>
+      <RouterProvider router={routes}></RouterProvider> 
+      
     </div>
   );
 }
