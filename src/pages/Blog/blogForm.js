d3ee16fabe1b4800b277734adeae776265223f20
Sakline-M <122230670+Sakline-M@users.noreply.github.com>
2023-07-12 16:43:31 +0600

    sakin blog page

---
+++ src/pages/Blog/blogForm.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const BlogForm = () => {
+  const navigate = useNavigate();
+  //All State
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  //create blog function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const blogData = new FormData();
+      blogData.append("name", name);
+      blogData.append("description", description);
+      blogData.append("photo", photo);
+      const { data } = axios.post(
+        `http://localhost:8080/api/v1/blog/blog-post`,
+        blogData
+      );
+      if (data?.success) {
+        toast.error(data?.message);
+      } else {
+        toast.success("Product Created Successfully");
+        navigate("/blog");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Somthing went wrong!");
+    }
+  };
+
+  return (
+    <>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+            <form>
+              <div className="card-body">
+                <h1 className="text-5xl">Create Your Blog</h1>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Blog Title"
+                  className="input shadow my-1"
+                  required
+                />
+
+                <textarea
+                  placeholder="Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="textarea textarea-bordered textarea-lg w-full max-w-xs"
+                ></textarea>
+
+                <div className="my-1">
+                  <label className="btn bnt-outline-secondary">
+                    {photo ? photo.name : "Upload Photo"}
+                    <input
+                      type="file"
+                      onChange={(e) => setPhoto(e.target.files[0])}
+                      hidden
+                      placeholder="img"
+                      accept="image/*"
+                      required
+                    />
+                  </label>
+                </div>
+                <div className="mb-3">
+                  {photo && (
+                    <div className="text-center">
+                      <img
+                        src={URL.createObjectURL(photo)}
+                        alt="blog-photo"
+                        height={"200"}
+                        className="img img-responsive"
+                      />
+                    </div>
+                  )}
+                </div>
+
+                <button
+                  onClick={handleSubmit}
+                  type="submit"
+                  className="btn bg-sky-400 mt-6"
+                >
+                  POST BLOG
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogForm;
---
+++ src/pages/Blog/blogPage.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  //get all blogs
+  const getAllBlogs = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/blog/get-blog`
+      );
+      setBlogs(data.blogs);
+    } catch (error) {
+      console.log(error);
+      toast.error("Somthing Went Wrong");
+    }
+  };
+  //use Effect
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
+
+  return (
+    <>
+      <div className="blog mt-20">
+        <div className="container">
+          <div className="row flex gap-2">
+            <div className="col-md-9 ">
+              {blogs?.map((p) => (
+                <div className=" w-full bg-base-100 shadow-xl">
+                  <figure>
+                    <img
+                      className=""
+                      width="1000"
+                      height="530"
+                      src={`http://localhost:8080/api/v1/blog/blog-photo/${p._id}`}
+                      alt="book"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title font-semibold text-4xl">
+                      {p.name}
+                    </h2>
+                    <p>July 11,2023</p>
+
+                    <div className="public-section flex">
+                      <a>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                          />
+                        </svg>
+                      </a>
+                      <a>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                          />
+                        </svg>
+                      </a>
+                      <a>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 6h.008v.008H6V6z"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                    <p>{p.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className="col-md-3 w-full">
+              <div className="popular-post">
+                <h1 className="text-xl font-bold">Popular Posts</h1>
+                <div className="div flex gap-1 mt-3">
+                  <img
+                    width="100"
+                    height="90"
+                    src="https://i.postimg.cc/3w0L3MMG/blog-featured-13.jpg"
+                    alt=""
+                  />
+                  <div className="h3">Experiences Through Education</div>
+                </div>
+              </div>
+              <div className="user-post mt-20 border-red-400 rounded">
+                <div className="text-xl font-semibold py-3">
+                  Publish your passions, your way
+                </div>
+                <div className="text-lg py-2">
+                  Create a unique and beautiful blog easily.
+                </div>
+                <div>
+                  <button className="btn  btn-warning mt-3">
+                    CREATE YOUR BLOG
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
